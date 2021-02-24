d43cb6f7ace5ab5d9703e2133f14d9cfa7917593
Darkhan <zhetkergen.dar@gmail.com>
2021-02-24 09:34:16 +0600

    fix

---
+++ JavaScript(Module7)/js/First_Problem.js
@@ -0,0 +1,15 @@
+const userDarkhan={
+    name:"Darkhan",
+    surname:"Zhetkergen",
+    age:19,
+    university:"Kbtu"
+}
+
+const userDaniyar={
+    name:"Daniyar",
+    surname:"Amangeldi",
+    age:21,
+    university:"Kbtu"
+}
+
+console.log(`First User:${userDarkhan.name},Second User:${userDaniyar.name}`)
